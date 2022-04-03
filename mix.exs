34a58636d743a723b6c866dfa4d8652079ea18bd
HenriqueArtur <henriqueacg@gmail.com>
2022-04-03 14:10:27 -0300

    :heavy_plus_sign: - change Poison to Jason

--- mix.exs
+++ mix.exs
@@ -21,10 +21,8 @@ defmodule MaxElixirPokeApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 1.7"},
-      {:poison, "~> 5.0"}
+      {:jason, "~> 1.2"}
     ]
   end
 end
