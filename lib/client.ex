8e8552fad68f84edb524a9482ffa99b3673dd697
HenriqueArtur <henriqueacg@gmail.com>
2022-04-02 22:25:41 -0300

    :recycle: - add tuples to manage errors

--- lib/client.ex
+++ lib/client.ex
@@ -1,13 +1,15 @@
 defmodule Client do
   alias Constant.URL, as: URL
+
   @base URL.get()
 
   def get(url) do
-    case HTTPoison.get(@base <> url) do
-      { :ok, %HTTPoison.Response{ body: body } } ->
-        body
-      { :error, %HTTPoison.Error{ reason: reason } } ->
-        { :error, reason }
-    end
+    @base <> url
+      |>HTTPoison.get()
+      |>response()
   end
+
+  defp response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
+  defp response({:ok, %{status_code: status_code}}), do: {:error, "HTTP Status '#{status_code}'"}
+  defp response({:error, %{reason: reason}}), do: {:error, reason}
 end
--- lib/get.ex
+++ lib/get.ex
@@ -2,25 +2,26 @@ defmodule Get do
   defmacro mget resource do
     quote do
       def get(id_or_name) do
-        id_or_name
-          |> get(:no_struct)
-          |> decode()
+        with {:ok, resouce_map} <- get(id_or_name, :no_struct) do
+          decode(resouce_map)
+        else
+          {:error, message} -> {:error, message}
+        end
       end
 
       def get(id_or_name, :no_struct) do
-        cond do
-          is_bitstring(id_or_name) ->
-            unquote(resource) <> URI.encode(id_or_name)
-              |> Client.get()
-              |> Poison.decode!()
-          is_integer(id_or_name) ->
-            unquote(resource) <> to_string(id_or_name)
-              |> Client.get()
-              |> Poison.decode!()
-          true ->
-            {:error, :wrong_entry}
+        with {:ok, url} <- generate_url(id_or_name),
+            {:ok, body} <- Client.get(url) do
+          resouce_map = Poison.decode!(body)
+          {:ok, resouce_map}
+        else
+          {:error, message} -> {:error, message}
         end
       end
+
+      defp generate_url(id_or_name) when is_bitstring(id_or_name), do: {:ok, unquote(resource) <> URI.encode(id_or_name)}
+      defp generate_url(id_or_name) when is_integer(id_or_name), do: {:ok, unquote(resource) <> to_string(id_or_name)}
+      defp generate_url(id_or_name), do: {:error, "Wrong entry '#{id_or_name}'"}
     end
   end
 end
