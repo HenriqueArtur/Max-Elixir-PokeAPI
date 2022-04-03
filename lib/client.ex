c55f3f2e5d969a1b1dcf64766eb479dcffb69677
HenriqueArtur <henriqueacg@gmail.com>
2022-04-03 14:25:50 -0300

    :recycle: - change Poison to Jason decode

--- lib/client.ex
+++ lib/client.ex
@@ -1,5 +1,6 @@
 defmodule Client do
   alias Constant.URL, as: URL
+  require Jason
 
   @base URL.get()
 
@@ -7,9 +8,18 @@ defmodule Client do
     @base <> url
       |>HTTPoison.get()
       |>response()
+      |>decode()
   end
 
   defp response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
   defp response({:ok, %{status_code: status_code}}), do: {:error, "HTTP Status '#{status_code}'"}
   defp response({:error, %{reason: reason}}), do: {:error, reason}
+
+  defp decode({:ok, body}) do
+    Jason.decode!(body)
+  end
+
+  defp decode({:error, _reason} = response) do
+    response
+  end
 end
