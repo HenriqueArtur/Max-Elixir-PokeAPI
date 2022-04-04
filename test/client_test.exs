aa45133fcf2485ce7877e6bdb9bb1e1be3b4357b
HenriqueArtur <henriqueacg@gmail.com>
2022-04-03 22:32:22 -0300

    :white_check_mark: - Client.get/1 return error

--- test/client_test.exs
+++ test/client_test.exs
@@ -1,81 +1,82 @@
 defmodule ClientTest do
   use ExUnit.Case
 
-  describe "get/1" do
-    test "return 200" do
-      param = "berry/1"
-      map = %{
-        "firmness" => %{
-          "name" => "soft",
-          "url" => "https://pokeapi.co/api/v2/berry-firmness/2/"
+  setup do
+    cheri_map = %{
+      "firmness" => %{
+        "name" => "soft",
+        "url" => "https://pokeapi.co/api/v2/berry-firmness/2/"
+      },
+      "flavors" => [
+        %{
+          "flavor" => %{
+            "name" => "spicy",
+            "url" => "https://pokeapi.co/api/v2/berry-flavor/1/"
+          },
+          "potency" => 10
         },
-        "flavors" => [
-          %{
-            "flavor" => %{
-              "name" => "spicy",
-              "url" => "https://pokeapi.co/api/v2/berry-flavor/1/"
-            },
-            "potency" => 10
+        %{
+          "flavor" => %{
+            "name" => "dry",
+            "url" => "https://pokeapi.co/api/v2/berry-flavor/2/"
           },
-          %{
-            "flavor" => %{
-              "name" => "dry",
-              "url" => "https://pokeapi.co/api/v2/berry-flavor/2/"
-            },
-            "potency" => 0
-          },
-          %{
-            "flavor" => %{
-              "name" => "sweet",
-              "url" => "https://pokeapi.co/api/v2/berry-flavor/3/"
-            },
-            "potency" => 0
-          },
-          %{
-            "flavor" => %{
-              "name" => "bitter",
-              "url" => "https://pokeapi.co/api/v2/berry-flavor/4/"
-            },
-            "potency" => 0
-          },
-          %{
-            "flavor" => %{
-              "name" => "sour",
-              "url" => "https://pokeapi.co/api/v2/berry-flavor/5/"
-            },
-            "potency" => 0
-          }
-        ],
-        "growth_time" => 3,
-        "id" => 1,
-        "item" => %{
-          "name" => "cheri-berry",
-          "url" => "https://pokeapi.co/api/v2/item/126/"
+          "potency" => 0
         },
-        "max_harvest" => 5,
-        "name" => "cheri",
-        "natural_gift_power" => 60,
-        "natural_gift_type" => %{
-          "name" => "fire",
-          "url" => "https://pokeapi.co/api/v2/type/10/"
+        %{
+          "flavor" => %{
+            "name" => "sweet",
+            "url" => "https://pokeapi.co/api/v2/berry-flavor/3/"
+          },
+          "potency" => 0
         },
-        "size" => 20,
-        "smoothness" => 25,
-        "soil_dryness" => 15
-      }
-      expected_response = {:ok, map}
-      response = Client.get(param)
-      assert response == expected_response
-    end
+        %{
+          "flavor" => %{
+            "name" => "bitter",
+            "url" => "https://pokeapi.co/api/v2/berry-flavor/4/"
+          },
+          "potency" => 0
+        },
+        %{
+          "flavor" => %{
+            "name" => "sour",
+            "url" => "https://pokeapi.co/api/v2/berry-flavor/5/"
+          },
+          "potency" => 0
+        }
+      ],
+      "growth_time" => 3,
+      "id" => 1,
+      "item" => %{
+        "name" => "cheri-berry",
+        "url" => "https://pokeapi.co/api/v2/item/126/"
+      },
+      "max_harvest" => 5,
+      "name" => "cheri",
+      "natural_gift_power" => 60,
+      "natural_gift_type" => %{
+        "name" => "fire",
+        "url" => "https://pokeapi.co/api/v2/type/10/"
+      },
+      "size" => 20,
+      "smoothness" => 25,
+      "soil_dryness" => 15
+    }
+
+    cheri_endpoint = "berry/1"
+
+    {:ok, cheri_map: cheri_map, cheri_endpoint: cheri_endpoint}
   end
 
-  describe "response/1" do
-    test "when status ok" do
-      body = "{\"firmness\":{\"name\":\"soft\",\"url\":\"https://pokeapi.co/api/v2/berry-firmness/2/\"},\"flavors\":[{\"flavor\":{\"name\":\"spicy\",\"url\":\"https://pokeapi.co/api/v2/berry-flavor/1/\"},\"potency\":10},{\"flavor\":{\"name\":\"dry\",\"url\":\"https://pokeapi.co/api/v2/berry-flavor/2/\"},\"potency\":0},{\"flavor\":{\"name\":\"sweet\",\"url\":\"https://pokeapi.co/api/v2/berry-flavor/3/\"},\"potency\":0},{\"flavor\":{\"name\":\"bitter\",\"url\":\"https://pokeapi.co/api/v2/berry-flavor/4/\"},\"potency\":0},{\"flavor\":{\"name\":\"sour\",\"url\":\"https://pokeapi.co/api/v2/berry-flavor/5/\"},\"potency\":0}],\"growth_time\":3,\"id\":1,\"item\":{\"name\":\"cheri-berry\",\"url\":\"https://pokeapi.co/api/v2/item/126/\"},\"max_harvest\":5,\"name\":\"cheri\",\"natural_gift_power\":60,\"natural_gift_type\":{\"name\":\"fire\",\"url\":\"https://pokeapi.co/api/v2/type/10/\"},\"size\":20,\"smoothness\":25,\"soil_dryness\":15}"
-      param = {:ok, %{status_code: 200, body: body}}
-      expected_response = {:ok, body}
-      response = Client.response(param)
+  describe "get/1" do
+    test "when has success [:ok]", %{cheri_map: map, cheri_endpoint: endpoint} do
+      expected_response = {:ok, map}
+      response = Client.get(endpoint)
       assert response == expected_response
+    end
+
+    test "when has fail [:error]" do
+      response = Client.get("banana/1")
+      assert {:error, _} = response
     end
   end
 end
