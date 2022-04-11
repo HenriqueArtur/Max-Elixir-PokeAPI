3796fa4fb6518729667a74f0225fb565a7af8f8d
HenriqueArtur <henriqueacg@gmail.com>
2022-04-10 23:01:35 -0300

    :memo: - add callbacks to Client

--- lib/client.ex
+++ lib/client.ex
@@ -2,6 +2,16 @@ defmodule Client do
   alias Constant.URL, as: URL
   require Jason
 
+  @callback get(url :: String.t) :: {:ok, %{}}
+  @callback get(url :: String.t) :: {:error, %{}}
+
+  @callback response({:ok, %{status_code: integer(), body: %{}}}) :: {:ok, body :: %{}}
+  @callback response({:ok, %{status_code: integer()}}) :: {:ok, %{reason: String.t}}
+  @callback response({:error, %{reason: String.t}}) :: {:ok, %{reason: String.t}}
+
+  @callback decode({:ok, %{}}) :: {:ok, %{}}
+  @callback decode({:error, %{reason: String.t}}) :: {:error, %{reason: String.t}}
+
   @base URL.get()
 
   def get(url) do
@@ -12,8 +22,10 @@ defmodule Client do
   end
 
   defp response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
-  defp response({:ok, %{status_code: status_code}}), do: {:error, "HTTP Status '#{status_code}'"}
-  defp response({:error, %{reason: reason}}), do: {:error, reason}
+
+  defp response({:ok, %{status_code: status_code}}), do: {:error, %{reason: "HTTP Status '#{status_code}'"}}
+
+  defp response({:error, %{reason: reason}}), do: {:error, %{reason: reason}}
 
   defp decode({:ok, body}) do
     map = Jason.decode!(body)
