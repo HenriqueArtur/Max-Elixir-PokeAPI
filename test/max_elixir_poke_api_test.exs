58980b8cced1be06b22748a92fb85e44034feaaf
Henrique Artur <contato@henriqueartur.com>
2022-12-26 18:23:22 -0300

    :white_check_mark: Add berry resource

---
+++ test/max_elixir_poke_api_test.exs
@@ -0,0 +1,37 @@
+defmodule MaxElixirPokeApiTest do
+  use ExUnit.Case
+  doctest MaxElixirPokeApi
+
+  @tag :pending
+  test "berry resource" do
+    berry_resource = {:ok,
+    %{
+      "count" => 64,
+      "next" => "https://pokeapi.co/api/v2/berry?offset=20&limit=20",
+      "previous" => nil,
+      "results" => [
+        %{"name" => "cheri", "url" => "https://pokeapi.co/api/v2/berry/1/"},
+        %{"name" => "chesto", "url" => "https://pokeapi.co/api/v2/berry/2/"},
+        %{"name" => "pecha", "url" => "https://pokeapi.co/api/v2/berry/3/"},
+        %{"name" => "rawst", "url" => "https://pokeapi.co/api/v2/berry/4/"},
+        %{"name" => "aspear", "url" => "https://pokeapi.co/api/v2/berry/5/"},
+        %{"name" => "leppa", "url" => "https://pokeapi.co/api/v2/berry/6/"},
+        %{"name" => "oran", "url" => "https://pokeapi.co/api/v2/berry/7/"},
+        %{"name" => "persim", "url" => "https://pokeapi.co/api/v2/berry/8/"},
+        %{"name" => "lum", "url" => "https://pokeapi.co/api/v2/berry/9/"},
+        %{"name" => "sitrus", "url" => "https://pokeapi.co/api/v2/berry/10/"},
+        %{"name" => "figy", "url" => "https://pokeapi.co/api/v2/berry/11/"},
+        %{"name" => "wiki", "url" => "https://pokeapi.co/api/v2/berry/12/"},
+        %{"name" => "mago", "url" => "https://pokeapi.co/api/v2/berry/13/"},
+        %{"name" => "aguav", "url" => "https://pokeapi.co/api/v2/berry/14/"},
+        %{"name" => "iapapa", "url" => "https://pokeapi.co/api/v2/berry/15/"},
+        %{"name" => "razz", "url" => "https://pokeapi.co/api/v2/berry/16/"},
+        %{"name" => "bluk", "url" => "https://pokeapi.co/api/v2/berry/17/"},
+        %{"name" => "nanab", "url" => "https://pokeapi.co/api/v2/berry/18/"},
+        %{"name" => "wepear", "url" => "https://pokeapi.co/api/v2/berry/19/"},
+        %{"name" => "pinap", "url" => "https://pokeapi.co/api/v2/berry/20/"}
+      ]
+    }}
+    assert MaxElixirPokeApi.resource(:berry) == berry_resource
+  end
+end
