d92d7a3f22911401db1dbb38ba018ca9e2e84999
Henrique Artur <contato@henriqueartur.com>
2022-12-26 17:39:20 -0300

    :necktie: init rebuild app from 0

--- lib/max_elixir_poke_api.ex
+++ lib/max_elixir_poke_api.ex
@@ -1,11 +1,22 @@
-defmodule MaxElixirPokeApi.Berry do
-  use MaxElixirPokeApi.Resources.Berry
-end
+defmodule MaxElixirPokeApi do
+  @url "https://pokeapi.co/api/v2/"
 
-defmodule MaxElixirPokeApi.BerryFlavor do
-  use MaxElixirPokeApi.Resources.BerryFlavor
-end
+  @http_client Application.get_env(:max_elixir_poke_api, :http_adapter, HTTPoison)
 
-defmodule MaxElixirPokeApi.BerryFirmness do
-  use MaxElixirPokeApi.Resources.BerryFirmness
+  def get(:berry) do
+    @url <> "berry"
+    |> @http_client.get()
+    |> get_response()
+    |> decode
+  end
+
+  defp get_response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
+  defp get_response({:ok, %{status_code: status_code}}), do: {:error, %{reason: "HTTP Status '#{status_code}'"}}
+  defp get_response({:error, reason: reason}), do: {:error, reason: reason}
+
+  defp decode({:error, _reason} = response), do: response
+  defp decode({:ok, body}) do
+    map = Jason.decode!(body)
+    {:ok, map}
+  end
 end
