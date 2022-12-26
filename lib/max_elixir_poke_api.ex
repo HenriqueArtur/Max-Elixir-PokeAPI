62544573cc483fb291b56c96355a6be7e755d5ea
Henrique Artur <contato@henriqueartur.com>
2022-12-26 18:24:57 -0300

    :recycle: split helpers funcs to do_request/1

--- lib/max_elixir_poke_api.ex
+++ lib/max_elixir_poke_api.ex
@@ -1,22 +1,13 @@
 defmodule MaxElixirPokeApi do
-  @url "https://pokeapi.co/api/v2/"
+  alias MaxElixirPokeApi.Helper
 
-  @http_client Application.get_env(:max_elixir_poke_api, :http_adapter, HTTPoison)
-
-  def get(:berry) do
-    @url <> "berry"
-    |> @http_client.get()
-    |> get_response()
-    |> decode
-  end
-
-  defp get_response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
-  defp get_response({:ok, %{status_code: status_code}}), do: {:error, %{reason: "HTTP Status '#{status_code}'"}}
-  defp get_response({:error, reason: reason}), do: {:error, reason: reason}
-
-  defp decode({:error, _reason} = response), do: response
-  defp decode({:ok, body}) do
-    map = Jason.decode!(body)
-    {:ok, map}
+  def resource(:berry), do: do_request("berry")
+  
+  defp do_request(resource) do
+    resource
+    |> Helper.make_url
+    |> Helper.client_get
+    |> Helper.response
+    |> Helper.decode
   end
 end
