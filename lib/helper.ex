cc1e337e3f0b62fc818f75a40f0d2b89d2f4a6b6
Henrique Artur <contato@henriqueartur.com>
2022-12-29 16:27:08 -0300

    :recycle: pass do_request/1 to Helper module

--- lib/helper.ex
+++ lib/helper.ex
@@ -2,16 +2,24 @@ defmodule MaxElixirPokeApi.Helper do
   @url "https://pokeapi.co/api/v2/"
   @http_client Application.get_env(:max_elixir_poke_api, :http_adapter, HTTPoison)
 
-  def make_url(resource), do: @url <> resource
+  def do_request(resource) do
+    resource
+    |> make_url
+    |> client_get
+    |> response
+    |> decode
+  end
 
-  def client_get(url), do: @http_client.get(url)
+  defp make_url(resource), do: @url <> resource
 
-  def response(tuple), do: do_response(tuple)
+  defp client_get(url), do: @http_client.get(url)
+
+  defp response(tuple), do: do_response(tuple)
   defp do_response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
   defp do_response({:ok, %{status_code: status_code}}), do: {:error, %{reason: "HTTP Status '#{status_code}'"}}
   defp do_response({:error, reason: reason}), do: {:error, reason: reason}
 
-  def decode(tuple), do: do_decode(tuple)
+  defp decode(tuple), do: do_decode(tuple)
   defp do_decode({:error, _reason} = response), do: response
   defp do_decode({:ok, body}), do: {:ok, Jason.decode!(body)}
 end
