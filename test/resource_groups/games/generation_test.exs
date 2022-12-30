a73c7f9dca8b111e8ea402a993540d17e9496a3e
Henrique Artur <contato@henriqueartur.com>
2022-12-30 14:17:05 -0300

    :white_check_mark: generation/1 pass seccess to test dir

--- test/resource_groups/games/generation_test.exs
+++ test/resource_groups/games/generation_test.exs
@@ -1,4 +1,18 @@
 defmodule MaxElixirPokeApi.GenerationTest do
   use ExUnit.Case, async: true
   doctest MaxElixirPokeApi.Generation
+
+  test "get/1 success" do
+    { :ok, resource } = MaxElixirPokeApi.Generation.get("generation-i")
+
+    assert resource["abilities"] |> is_list
+    assert resource["id"] |> is_integer
+    assert resource["main_region"] |> is_map
+    assert resource["moves"] |> is_list
+    assert resource["name"] |> is_bitstring
+    assert resource["names"] |> is_list
+    assert resource["pokemon_species"] |> is_list
+    assert resource["types"] |> is_list
+    assert resource["version_groups"] |> is_list
+  end
 end
