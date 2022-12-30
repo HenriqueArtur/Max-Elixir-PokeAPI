0ab7831543a862cd5282622e7b982b0532ab3a8c
Henrique Artur <contato@henriqueartur.com>
2022-12-29 21:38:20 -0300

    :white_check_mark: doctests berry/1

--- lib/max_elixir_poke_api.ex
+++ lib/max_elixir_poke_api.ex
@@ -134,6 +134,76 @@ defmodule MaxElixirPokeApi do
   ## Parameters
 
     - id_or_name: Integer or String that represents the resource identify. `https://pokeapi.co/api/v2/berry/{id or name}/`
+
+  ## Examples
+
+      iex> MaxElixirPokeApi.berry("cheri")
+      {:ok,
+        %{
+          "firmness" => %{
+            "name" => "soft",
+            "url" => "https://pokeapi.co/api/v2/berry-firmness/2/"
+          },
+          "flavors" => [
+            %{
+              "flavor" => %{
+                "name" => "spicy",
+                "url" => "https://pokeapi.co/api/v2/berry-flavor/1/"
+              },
+              "potency" => 10
+            },
+            %{
+              "flavor" => %{
+                "name" => "dry",
+                "url" => "https://pokeapi.co/api/v2/berry-flavor/2/"
+              },
+              "potency" => 0
+            },
+            %{
+              "flavor" => %{
+                "name" => "sweet",
+                "url" => "https://pokeapi.co/api/v2/berry-flavor/3/"
+              },
+              "potency" => 0
+            },
+            %{
+              "flavor" => %{
+                "name" => "bitter",
+                "url" => "https://pokeapi.co/api/v2/berry-flavor/4/"
+              },
+              "potency" => 0
+            },
+            %{
+              "flavor" => %{
+                "name" => "sour",
+                "url" => "https://pokeapi.co/api/v2/berry-flavor/5/"
+              },
+              "potency" => 0
+            }
+          ],
+          "growth_time" => 3,
+          "id" => 1,
+          "item" => %{
+            "name" => "cheri-berry",
+            "url" => "https://pokeapi.co/api/v2/item/126/"
+          },
+          "max_harvest" => 5,
+          "name" => "cheri",
+          "natural_gift_power" => 60,
+          "natural_gift_type" => %{
+            "name" => "fire",
+            "url" => "https://pokeapi.co/api/v2/type/10/"
+          },
+          "size" => 20,
+          "smoothness" => 25,
+          "soil_dryness" => 15
+        }}
+
+      iex> MaxElixirPokeApi.berry(101)
+      {:error, %{reason: "HTTP Status '404'"}}
+
+      iex> MaxElixirPokeApi.berry(:banana)
+      {:error, %{reason: "&1 only accepts string or integer."}}
   """
   @spec berry(id_or_name) :: {:ok, map} | {:error, %{reason: String.t()}}
   def berry(id_or_name), do: Berry.get(id_or_name)
