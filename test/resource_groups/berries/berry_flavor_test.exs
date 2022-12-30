1cc3b13a25631ae6dcf1825981b1418b0942f735
Henrique Artur <contato@henriqueartur.com>
2022-12-29 22:44:26 -0300

    :white_check_mark: add doctest to resources

---
+++ test/resource_groups/berries/berry_flavor_test.exs
@@ -0,0 +1,4 @@
+defmodule MaxElixirPokeApi.BerryFlavorTest do
+  use ExUnit.Case, async: true
+  doctest MaxElixirPokeApi.BerryFlavor
+end
---
+++ test/resource_groups/resource_test.exs
@@ -0,0 +1,4 @@
+defmodule MaxElixirPokeApi.ResourceTest do
+  use ExUnit.Case, async: true
+  doctest MaxElixirPokeApi.Resource
+end
