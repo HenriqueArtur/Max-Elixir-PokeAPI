afde60fa87f50147433db9247861945bab92e88b
Henrique Artur <contato@henriqueartur.com>
2023-07-15 21:04:34 -0300

    :heavy_plus_sign: Mox

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule MaxElixirPokeApi.MixProject do
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.2"},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:cachex, "~> 3.4"}
+      {:cachex, "~> 3.4"},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
