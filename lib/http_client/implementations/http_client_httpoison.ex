42216fcd59c1c7cecfc2e9c0fb0af34cf982447e
Henrique Artur <contato@henriqueartur.com>
2023-07-23 17:32:21 -0300

    :recycle: Create http client as httppoison

---
+++ lib/http_client/implementations/http_client_httpoison.ex
@@ -0,0 +1,22 @@
+defmodule MaxElixirPokeApi.HttpClient.HTTPoison do
+  @behaviour MaxElixirPokeApi.HttpClient
+
+  @impl true
+  def get(url) do
+    url
+    |> get_url
+    |> response
+    |> decode
+  end
+
+  defp get_url(url), do: HTTPoison.get(url)
+
+  @doc false
+  defp response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
+  defp response({:ok, %{status_code: status_code}}), do: {:error, %{reason: "HTTP Status '#{status_code}'"}}
+  defp response({:error, reason} = err), do: err
+
+  @doc false
+  defp decode({:error, _reason} = response), do: response
+  defp decode({:ok, body}), do: {:ok, Jason.decode!(body)}
+end
