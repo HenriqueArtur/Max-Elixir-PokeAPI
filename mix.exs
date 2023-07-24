66dd3e028e0aeb899a229511a241db2b82ebec5e
Henrique Artur <contato@henriqueartur.com>
2023-07-23 22:53:07 -0300

    :bookmark: V1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MaxElixirPokeApi.MixProject do
   def project do
     [
       app: :max_elixir_poke_api,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       deps: deps(),
       start_permanent: Mix.env() == :prod,
